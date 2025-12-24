cbdf9a4e30a9c42a4c9b2539b9279040ca529ca3
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-12-24 15:56:09 +0530

    update

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersRepository } from './users.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  imports: [TypeOrmModule.forFeature([UsersRepository])],
+  providers: [AuthService, UsersRepository],
+  controllers: [AuthController],
 })
 export class AuthModule {}
